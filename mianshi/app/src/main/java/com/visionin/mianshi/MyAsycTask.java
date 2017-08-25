7fb801245f26e291f5a960c3c38c67166a712e8d
xiaoxiaopolang <1014008793@qq.com>
2017-08-25 14:26:51 +0800

    add android

---
+++ mianshi/app/src/main/java/com/visionin/mianshi/MyAsycTask.java
@@ -0,0 +1,30 @@
+package com.visionin.mianshi;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by wangzhiyuan on 2017/8/25.
+ */
+
+public class MyAsycTask extends AsyncTask {
+
+    @Override
+    protected void onProgressUpdate(Object[] values) {
+        super.onProgressUpdate(values);
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected Object doInBackground(Object[] objects) {
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Object o) {
+        super.onPostExecute(o);
+    }
+}
